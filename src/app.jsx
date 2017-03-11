8697b15d18b1a39501615808455ab7d0c5086b86
Joel Peterson <jpeters5392@gmail.com>
2017-03-11 11:21:16 -0600

    set up initial project and build

---
+++ src/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+    <div>
+        <h1>React Training</h1>
+    </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('appRoot'));
